4bf4a9983bdc4b4a683a7d487462b73332a87ca0
Tom-w-Lee <wl6g11@163.com>
2018-03-05 12:33:08 +0800

    测试latex问题

--- source/conf.py
+++ source/conf.py
@@ -224,10 +224,12 @@ latex_elements = {
 'pointsize': '10pt',
 
 # Additional stuff for the LaTeX preamble.
-#'preamble': '',
+'preamble': r'''
+\usepackage[utf8]{inputenc}
 
 # Latex figure (float) alignment
 #'figure_align': 'htbp',
+
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
